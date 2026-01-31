e0f7d2e148aaabac6da9da9517c1decf578dfbaf
mustafaelbaz5 <m9stafa05@gmail.com>
2026-01-31 15:25:26 +0200

    Refactor By Adding Sign up Button To the Sign Up Form

--- lib/core/auth/ui/sign_up_screen.dart
+++ lib/core/auth/ui/sign_up_screen.dart
@@ -2,11 +2,7 @@ import 'package:doc_doc/core/auth/ui/widgets/already_have_account_text.dart';
 import 'package:doc_doc/core/auth/ui/widgets/auth_screens_header.dart';
 import 'package:doc_doc/core/auth/ui/widgets/sign_up_form.dart';
 import 'package:doc_doc/core/auth/ui/widgets/terms_and_conditions.dart';
-import 'package:doc_doc/core/extensions/context_extensions.dart';
-import 'package:doc_doc/core/router/routes.dart';
-import 'package:doc_doc/core/themes/app_text_styles.dart';
 import 'package:doc_doc/core/utils/spacing.dart';
-import 'package:doc_doc/core/widgets/custom_text_button.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -30,16 +26,6 @@ class SignUpScreen extends StatelessWidget {
                 ),
                 verticalSpacing(60),
                 const SignUpForm(),
-                verticalSpacing(40),
-                CustomTextButton(
-                  borderRadius: responsiveRadius(16),
-                  text: 'auth.sign_up'.tr(),
-                  textStyle: AppTextStyles.font18SemiBold,
-                  style: CustomButtonStyle.filled,
-                  size: CustomButtonSize.large,
-                  onPressed: () =>
-                      context.pushNamedAndRemoveAll(Routes.homeScreen),
-                ),
                 verticalSpacing(50),
                 const TermsAndConditions(),
                 verticalSpacing(60),
