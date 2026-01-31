9db02da893e00d6785a06114f9cead015bcb2a00
mustafaelbaz5 <m9stafa05@gmail.com>
2026-01-31 03:28:16 +0200

    Set storage Constants Key

--- lib/core/constants/storage_constants.dart
+++ lib/core/constants/storage_constants.dart
@@ -2,7 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
-class StorageConstants {}
+class StorageConstants {
+  static const String userTokenKey = 'user_token';
+}
 
 Future<void> setupHydratedStorage() async {
   HydratedBloc.storage = await HydratedStorage.build(
