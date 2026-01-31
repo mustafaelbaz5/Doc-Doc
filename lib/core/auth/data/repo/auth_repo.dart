b9bf6993f4f93079904b98d7887c89c510c38338
mustafaelbaz5 <m9stafa05@gmail.com>
2026-01-31 15:21:17 +0200

    Build And Impl Sign Up Repo

--- lib/core/auth/data/repo/auth_repo.dart
+++ lib/core/auth/data/repo/auth_repo.dart
@@ -1,6 +1,10 @@
 import 'package:doc_doc/core/auth/data/models/login_request_body.dart';
 import 'package:doc_doc/core/auth/data/models/login_response_body.dart';
+import 'package:doc_doc/core/auth/data/models/sign_up_request_body.dart';
+import 'package:doc_doc/core/auth/data/models/sign_up_response_body.dart';
 
 abstract class AuthRepo {
-  Future<LoginResponseBody> login(final LoginRequestBody loginRequestBody);
+  Future<LoginResponseBody> login(final LoginRequestBody body);
+
+  Future<SignUpResponseBody> signUp(final SignUpRequestBody body);
 }
--- lib/core/auth/data/repo/auth_repo_impl.dart
+++ lib/core/auth/data/repo/auth_repo_impl.dart
@@ -1,6 +1,8 @@
 import 'package:doc_doc/core/auth/data/apis/auth_service.dart';
 import 'package:doc_doc/core/auth/data/models/login_request_body.dart';
 import 'package:doc_doc/core/auth/data/models/login_response_body.dart';
+import 'package:doc_doc/core/auth/data/models/sign_up_request_body.dart';
+import 'package:doc_doc/core/auth/data/models/sign_up_response_body.dart';
 import 'package:doc_doc/core/auth/data/repo/auth_repo.dart';
 import 'package:doc_doc/core/error/types/error_handler.dart';
 
@@ -9,9 +11,19 @@ class AuthRepoImpl implements AuthRepo {
 
   AuthRepoImpl({required this.authService});
   @override
-  Future<LoginResponseBody> login(final LoginRequestBody loginRequestBody) {
+  Future<LoginResponseBody> login(final LoginRequestBody body) {
     try {
-      final response = authService.login(loginRequestBody);
+      final response = authService.login(body);
+      return response;
+    } catch (e) {
+      ErrorHandler.handle(e);
+    }
+  }
+
+  @override
+  Future<SignUpResponseBody> signUp(final SignUpRequestBody body) {
+    try {
+      final response = authService.signUp(body);
       return response;
     } catch (e) {
       ErrorHandler.handle(e);
