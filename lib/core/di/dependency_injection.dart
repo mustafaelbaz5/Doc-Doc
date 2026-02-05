6fe3a0a914022e4cd7aafe0e0f42be7335fba847
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-05 02:46:14 +0200

    Implement Dependency Injection for Home Module Components

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,12 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:doc_doc/modules/users/features/home/data/remote/home_remote_api.dart';
+import 'package:doc_doc/modules/users/features/home/data/repo/home_repo.dart';
+import 'package:doc_doc/modules/users/features/home/data/repo/home_repo_impl.dart';
+import 'package:doc_doc/modules/users/features/home/logic/cubit/home_cubit.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
+
 import '../auth/data/remote/auth_remote_api.dart';
 import '../auth/data/repo/auth_repo.dart';
 import '../auth/data/repo/auth_repo_impl.dart';
 import '../auth/logic/cubit/auth_cubit.dart';
 import '../networking/dio_factory.dart';
 import '../service/secure_storage.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -21,7 +26,7 @@ Future<void> setUpDependencies() async {
   }
 
   final Dio dio = await DioFactory.getDio();
-
+  // Auth DI
   if (!getIt.isRegistered<AuthRemoteApi>()) {
     getIt.registerLazySingleton<AuthRemoteApi>(() => AuthRemoteApi(dio: dio));
   }
@@ -38,6 +43,23 @@ Future<void> setUpDependencies() async {
   if (!getIt.isRegistered<AuthCubit>()) {
     getIt.registerFactory<AuthCubit>(
       () => AuthCubit(authRepo: getIt<AuthRepo>()),
+    );
+  }
+
+  // Home DI
+  if (!getIt.isRegistered<HomeRemoteApi>()) {
+    getIt.registerLazySingleton<HomeRemoteApi>(() => HomeRemoteApi(dio: dio));
+  }
+
+  if (!getIt.isRegistered<HomeRepo>()) {
+    getIt.registerLazySingleton<HomeRepo>(
+      () => HomeRepoImpl(homeRemoteApi: getIt<HomeRemoteApi>()),
+    );
+  }
+
+  if (!getIt.isRegistered<HomeCubit>()) {
+    getIt.registerFactory<HomeCubit>(
+      () => HomeCubit(homeRepo: getIt<HomeRepo>()),
     );
   }
 }
