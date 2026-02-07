146b4543dce80552b3a5d127a06713be0b7b3a80
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-07 06:29:57 +0200

    Wrap SpecialtiesGridView withBlocBuilder for mange SpecialtiesGridView and stats

--- lib/modules/users/features/doctor_specialty/ui/widgets/specialties_grid_view.dart
+++ lib/modules/users/features/doctor_specialty/ui/widgets/specialties_grid_view.dart
@@ -1,56 +1,54 @@
 import 'package:doc_doc/core/models/specialization_data_model.dart';
+import 'package:doc_doc/modules/users/features/doctor_specialty/logic/cubit/specialty_cubit.dart';
 import 'package:doc_doc/modules/users/features/doctor_specialty/ui/widgets/specialty_grid_view_item.dart';
+import 'package:doc_doc/modules/users/features/doctor_specialty/ui/widgets/specialty_shimmer_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SpecialtiesGridView extends StatefulWidget {
-  const SpecialtiesGridView({super.key, this.onItemTap});
-
-  final Function(int)? onItemTap;
-
-  @override
-  State<SpecialtiesGridView> createState() => _SpecialtiesGridViewState();
-}
-
-class _SpecialtiesGridViewState extends State<SpecialtiesGridView> {
-  int selectedIndex = -1;
-
-  final List<SpecializationDataModel> specializations = [
-    SpecializationDataModel(id: 1, name: 'Cardiology', doctors: []),
-    SpecializationDataModel(id: 2, name: 'Dermatology', doctors: []),
-    SpecializationDataModel(id: 3, name: 'Neurology', doctors: []),
-    SpecializationDataModel(id: 4, name: 'Pediatrics', doctors: []),
-    SpecializationDataModel(id: 5, name: 'Psychiatry', doctors: []),
-    SpecializationDataModel(id: 6, name: 'Radiology', doctors: []),
-    SpecializationDataModel(id: 6, name: 'Radiology', doctors: []),
-  ];
+class SpecialtiesGridView extends StatelessWidget {
+  const SpecialtiesGridView({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: GridView.builder(
-          itemCount: specializations.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // 3 items per row
-            crossAxisSpacing: 24, // horizontal spacing
-            mainAxisSpacing: 16, // vertical spacing
-            childAspectRatio: .8, // width/height ratio
-          ),
-          itemBuilder: (final context, final index) {
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-              child: SpecialtyGridViewItem(
-                specializationsData: specializations[index],
+    return BlocBuilder<SpecialtyCubit, SpecialtyState>(
+      builder: (final context, final state) {
+        if (state is SpecialtyLoading) {
+          return const SpecialtyShimmerLoading();
+        } else if (state is SpecialtyFailed) {
+          return Center(child: Text(state.error.messageKey));
+        } else if (state is AllSpecializationLoaded) {
+          final List<SpecializationDataModel> specializations =
+              state.specializations!;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: GridView.builder(
+              shrinkWrap: true, // important for vertical scroll
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemCount: specializations.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 3 items per row
+                crossAxisSpacing: 28, // horizontal spacing
+                mainAxisSpacing: 16, // vertical spacing
+                childAspectRatio: .75, // width/height ratio
               ),
-            );
-          },
-        ),
-      ),
+              itemBuilder: (final context, final index) {
+                return GestureDetector(
+                  onTap: () {
+                    context.read<SpecialtyCubit>().getSpecialtyById(
+                      specialtyId: specializations[index].id,
+                    );
+                  },
+                  child: SpecialtyGridViewItem(
+                    specializationsData: specializations[index],
+                  ),
+                );
+              },
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
     );
   }
 }
