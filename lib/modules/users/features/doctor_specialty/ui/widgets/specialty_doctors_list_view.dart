ef91fb2f20084ab97f77cd9762a258d753955540
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-07 07:33:31 +0200

    Build SpecialtyDoctorsListView Ui With Provideing BlocBuilder for managing Ui And States

---
+++ lib/modules/users/features/doctor_specialty/ui/widgets/specialty_doctors_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:doc_doc/core/models/doctor_data_model.dart';
+import 'package:doc_doc/core/utils/spacing.dart';
+import 'package:doc_doc/modules/users/features/doctor_specialty/logic/cubit/specialty_cubit.dart';
+import 'package:doc_doc/modules/users/features/doctor_specialty/ui/widgets/specialty_doctors_list_view_card.dart';
+import 'package:doc_doc/modules/users/features/doctor_specialty/ui/widgets/specialty_doctors_shimmer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SpecialtyDoctorsListView extends StatelessWidget {
+  const SpecialtyDoctorsListView({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return Expanded(
+      child: BlocBuilder<SpecialtyCubit, SpecialtyState>(
+        builder: (final context, final state) {
+          if (state is SpecialtyLoading) {
+            return const Center(child: SpecialtyDoctorsShimmer());
+          } else if (state is SpecialtyFailed) {
+            return Center(child: Text(state.error.messageKey));
+          } else if (state is SpecialtyLoaded) {
+            final List<DoctorDataModel> doctors = state.doctorsList!;
+            return ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: responsiveWidth(8)),
+              itemCount: doctors.length,
+              itemBuilder: (final context, final index) {
+                final doctor = doctors[index];
+                return Padding(
+                  padding: EdgeInsets.only(bottom: responsiveHeight(16)),
+                  child: SpecialtyDoctorsListViewCard(doctor: doctor),
+                );
+              },
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
