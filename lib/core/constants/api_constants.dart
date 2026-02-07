d1e9bef795e44575ada1e6db98a1d69f397341dd
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-07 06:27:48 +0200

    Set Specialty End Points & Build Specialty api Calls

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,15 @@
 class ApiConstants {
   static const String apiBaseUrl = 'https://vcare.integration25.com/api/';
+
+  // Authentication
   static const String loginEndpoint = 'auth/login';
   static const String signUpEndpoint = 'auth/register';
-  static const String logoutEndpoint = 'auth/login';
+  static const String logoutEndpoint = 'auth/logout';
+
+  // Home
   static const String homeSpecializationEndPoint = 'home/index';
+
+  // Specialization
+  static const String allSpecializationEndpoint = 'specialization/index';
+  static const String showSpecialtyEndpoint = 'specialization/show/';
 }
---
+++ lib/modules/users/features/doctor_specialty/data/remote/specialty_remote_api.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:doc_doc/core/constants/api_constants.dart';
+import 'package:doc_doc/core/models/specialization_data_model.dart';
+import 'package:doc_doc/core/models/specializations_response_body.dart';
+
+class SpecialtyRemoteApi {
+  final Dio dio;
+  final String _baseUrl = ApiConstants.apiBaseUrl;
+
+  SpecialtyRemoteApi({required this.dio});
+
+  Future<SpecializationsResponseBody> getAllSpecialization() async {
+    final response = await dio.get(
+      '$_baseUrl${ApiConstants.allSpecializationEndpoint}',
+    );
+    return SpecializationsResponseBody.fromJson(response.data);
+  }
+
+  Future<SpecializationDataModel> getSpecialtyById({
+    required final int id,
+  }) async {
+    final response = await dio.get(
+      '$_baseUrl${ApiConstants.showSpecialtyEndpoint}$id',
+    );
+    return SpecializationDataModel.fromJson(response.data);
+  }
+}
