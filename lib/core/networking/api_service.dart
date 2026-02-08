47bde9447f3726286097e51436dc0a87b83ff93c
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-08 09:32:54 +0200

    Build ApiService For all Api Call Methods

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:doc_doc/core/error/types/error_handler.dart';
+import 'package:doc_doc/core/networking/api_end_points.dart';
+
+class ApiService {
+  final Dio dio;
+
+  ApiService({required this.dio});
+
+  String get _baseUrl => ApiEndPoints.apiBaseUrl;
+
+  Future<T> get<T>({
+    required final String endPoint,
+    final Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await dio.get<T>(
+        _baseUrl + endPoint,
+        queryParameters: queryParameters,
+      );
+      return response.data as T;
+    } catch (e) {
+      ErrorHandler.handle(e);
+    }
+  }
+
+  Future<T> post<T>({
+    required final String endPoint,
+    final dynamic data,
+    final Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await dio.post<T>(
+        _baseUrl + endPoint,
+        data: data,
+        queryParameters: queryParameters,
+      );
+      return response.data as T;
+    } catch (e) {
+      ErrorHandler.handle(e);
+    }
+  }
+
+  Future<T> put<T>({required final String endPoint, final dynamic data}) async {
+    try {
+      final response = await dio.put<T>(_baseUrl + endPoint, data: data);
+      return response.data as T;
+    } catch (e) {
+      ErrorHandler.handle(e);
+    }
+  }
+
+  Future<T> delete<T>({
+    required final String endPoint,
+    final dynamic data,
+  }) async {
+    try {
+      final response = await dio.delete<T>(_baseUrl + endPoint, data: data);
+      return response.data as T;
+    } catch (e) {
+      ErrorHandler.handle(e);
+    }
+  }
+}
