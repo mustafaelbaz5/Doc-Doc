c537d58cd01ce9d17451243d2d30165f674d68e1
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-18 07:31:39 +0200

    Refactor Login And Sign Up Screen To ser blocConsumer to all the screen ui

--- lib/core/auth/ui/widgets/login/login_form.dart
+++ lib/core/auth/ui/widgets/login/login_form.dart
@@ -2,11 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../extensions/context_extensions.dart';
-import '../../../../router/routes.dart';
 import '../../../../themes/app_text_styles.dart';
-import '../../../../ui/dialogs/app_dialogs.dart';
-import '../../../../ui/loaders/overlay_loader.dart';
 import '../../../../utils/spacing.dart';
 import '../../../../utils/validators.dart';
 import '../../../../widgets/custom_text_button.dart';
@@ -52,52 +48,38 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(final BuildContext context) {
-    return BlocConsumer<AuthCubit, AuthState>(
-      listener: (final context, final state) {
-        if (state is AuthAuthenticated) {
-          context.pushNamedAndRemoveAll(Routes.mainScaffold);
-        } else if (state is AuthFailure) {
-          AppDialogs.showError(context, message: state.error.messageKey.tr());
-        }
-      },
-      builder: (final context, final state) {
-        return OverlayLoader(
-          isLoading: state is AuthChecking,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  controller: _emailController,
-                  hintText: "auth.email".tr(),
-                  validator: Validators.email,
-                ),
-                verticalSpacing(16),
-                CustomTextFormField(
-                  controller: _passwordController,
-                  hintText: "auth.password".tr(),
-                  isPassword: true,
-                  validator: (final value) {
-                    if (value == null || value.isEmpty) {
-                      return "auth.validation.required".tr();
-                    }
-                    return null;
-                  },
-                ),
-                verticalSpacing(32),
-                CustomTextButton(
-                  borderRadius: responsiveRadius(16),
-                  text: "auth.login".tr(),
-                  textStyle: AppTextStyles.font18SemiBold,
-                  style: CustomButtonStyle.filled,
-                  size: CustomButtonSize.large,
-                  onPressed: _submitForm,
-                ),
-              ],
-            ),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField(
+            controller: _emailController,
+            hintText: "auth.email".tr(),
+            validator: Validators.email,
           ),
-        );
-      },
+          verticalSpacing(16),
+          CustomTextFormField(
+            controller: _passwordController,
+            hintText: "auth.password".tr(),
+            isPassword: true,
+            validator: (final value) {
+              if (value == null || value.isEmpty) {
+                return "auth.validation.required".tr();
+              }
+              return null;
+            },
+          ),
+          verticalSpacing(32),
+          CustomTextButton(
+            borderRadius: responsiveRadius(16),
+            text: "auth.login".tr(),
+            textStyle: AppTextStyles.font18SemiBold,
+            style: CustomButtonStyle.filled,
+            size: CustomButtonSize.large,
+            onPressed: _submitForm,
+          ),
+        ],
+      ),
     );
   }
 }
