f190dfa9d1ee29b5a3550391895f88dbb7281f10
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-18 07:35:13 +0200

    Handle Home getHomeSpecializations method to remove duplicate request

--- lib/modules/users/features/home/ui/home_screen.dart
+++ lib/modules/users/features/home/ui/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:doc_doc/modules/users/features/home/logic/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/utils/spacing.dart';
 import 'widgets/doctor_recommendation/home_recommendation_doctor.dart';
@@ -6,8 +8,23 @@ import 'widgets/doctor_specialty/home_doctor_specialty.dart';
 import 'widgets/doctors_blue_container.dart';
 import 'widgets/home_app_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        context.read<HomeCubit>().getHomeSpecializations();
+      }
+    });
+  }
 
   @override
   Widget build(final BuildContext context) {
@@ -22,7 +39,6 @@ class HomeScreen extends StatelessWidget {
             verticalSpacing(24),
             const HomeDoctorSpecialty(),
             verticalSpacing(24),
-            // Give it fixed height or Expanded
             const Expanded(child: HomeRecommendationDoctor()),
           ],
         ),
