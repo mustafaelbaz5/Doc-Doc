1f025258a1fff97fcf30a0b06c28dc1915c0ddca
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-22 05:35:47 +0200

    Build Filterr Data NAd cubit for filter data

---
+++ lib/modules/users/features/all_doctors/logic/cubit/filter_data/filter_data_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:doc_doc/core/error/models/app_error.dart';
+import 'package:doc_doc/core/models/city_data_model.dart';
+import 'package:doc_doc/core/models/specialization_data_model.dart';
+import 'package:doc_doc/modules/users/features/all_doctors/data/repo/all_doctors_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'filter_data_state.dart';
+
+class FilterDataCubit extends Cubit<FilterDataState> {
+  final AllDoctorsRepo allDoctorsRepo;
+
+  FilterDataCubit({required this.allDoctorsRepo}) : super(FilterDataInitial());
+  Future<void> loadFilterData() async {
+    emit(FilterDataLoading());
+    final specializations = await allDoctorsRepo.getSpecializations();
+    final cities = await allDoctorsRepo.getCities();
+    emit(
+      FilterDataLoaded(
+        specializations: specializations.data,
+        cities: cities,
+      ),
+    );
+  }
+}
