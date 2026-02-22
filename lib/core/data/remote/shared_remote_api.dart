31bc1dd7fa1812acecae3ae5c11b968e22de2ade
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-22 03:30:38 +0200

    Craate SharedRemoteApi for Shared api methods

---
+++ lib/core/data/remote/shared_remote_api.dart
@@ -0,0 +1,25 @@
+import 'package:doc_doc/core/models/city_data_model.dart';
+import 'package:doc_doc/core/models/specializations_response_body.dart';
+import 'package:doc_doc/core/networking/api_end_points.dart';
+import 'package:doc_doc/core/networking/api_service.dart';
+
+class SharedRemoteApi {
+  final ApiService apiService;
+  SharedRemoteApi({required this.apiService});
+
+  Future<SpecializationsResponseBody> getSpecializations() async {
+    final data = await apiService.get<Map<String, dynamic>>(
+      endPoint: ApiEndPoints.allSpecializationEndpoint,
+    );
+    return SpecializationsResponseBody.fromJson(data);
+  }
+
+  Future<List<CityDataModel>> getCities() async {
+    final response = await apiService.get<Map<String, dynamic>>(
+      endPoint: ApiEndPoints.citiesEndPoint,
+    );
+    return (response['data'] as List)
+        .map((final item) => CityDataModel.fromJson(item))
+        .toList();
+  }
+}
--- lib/modules/users/features/doctor_specialty/data/repo/specialty_repo_impl.dart
+++ lib/modules/users/features/doctor_specialty/data/repo/specialty_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'package:doc_doc/core/data/remote/shared_remote_api.dart';
+
 import '../../../../../../core/models/specialization_data_model.dart';
 import '../../../../../../core/models/specializations_response_body.dart';
 import '../remote/specialty_remote_api.dart';
@@ -5,11 +7,15 @@ import 'specialty_repo.dart';
 
 class SpecialtyRepoImpl implements SpecialtyRepo {
   final SpecialtyRemoteApi specialtyRemoteApi;
+  final SharedRemoteApi sharedRemoteApi;
 
-  SpecialtyRepoImpl({required this.specialtyRemoteApi});
+  SpecialtyRepoImpl({
+    required this.specialtyRemoteApi,
+    required this.sharedRemoteApi,
+  });
   @override
   Future<SpecializationsResponseBody> getAllSpecialization() async {
-    return await specialtyRemoteApi.getAllSpecialization();
+    return await sharedRemoteApi.getSpecializations();
   }
 
   @override
