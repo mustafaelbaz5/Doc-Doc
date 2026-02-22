9f8abd4ccb0bf00ba7fe11d0ea7cc52de7a6bacf
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-22 05:35:20 +0200

    Move the doctor card to the core

--- lib/core/widgets/doctor_list_view_card.dart
+++ lib/core/widgets/doctor_list_view_card.dart
@@ -1,14 +1,13 @@
+import 'package:doc_doc/core/extensions/context_extensions.dart';
+import 'package:doc_doc/core/models/doctor_data_model.dart';
+import 'package:doc_doc/core/themes/app_colors.dart';
+import 'package:doc_doc/core/themes/app_text_styles.dart';
+import 'package:doc_doc/core/utils/app_assets.dart';
+import 'package:doc_doc/core/utils/spacing.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../../../core/extensions/context_extensions.dart';
-import '../../../../../../../core/models/doctor_data_model.dart';
-import '../../../../../../../core/themes/app_colors.dart';
-import '../../../../../../../core/themes/app_text_styles.dart';
-import '../../../../../../../core/utils/app_assets.dart';
-import '../../../../../../../core/utils/spacing.dart';
-
-class DoctorRecommendationCard extends StatelessWidget {
-  const DoctorRecommendationCard({super.key, required this.doctor, this.onTap});
+class DoctorListViewCard extends StatelessWidget {
+  const DoctorListViewCard({super.key, required this.doctor, this.onTap});
 
   final DoctorDataModel doctor;
   final VoidCallback? onTap;
