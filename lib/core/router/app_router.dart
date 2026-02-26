993aad4c16ef7908b0919852d3307c24e7b4e477
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-26 02:39:07 +0200

    Set Doctors deatils Route &Update Route name

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,17 +1,19 @@
-import '../../modules/users/features/all_doctors/logic/all_doctors/all_doctors_cubit.dart';
-import '../../modules/users/features/all_doctors/logic/filter_data/filter_data_cubit.dart';
-import '../../modules/users/features/all_doctors/ui/all_doctors_screen.dart';
+import 'package:doc_doc/modules/users/features/doctor_details/logic/cubit/doctor_details_cubit.dart';
+import 'package:doc_doc/modules/users/features/doctor_details/ui/doctor_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../modules/users/features/all_doctors/logic/all_doctors/all_doctors_cubit.dart';
+import '../../modules/users/features/all_doctors/logic/filter_data/filter_data_cubit.dart';
+import '../../modules/users/features/all_doctors/ui/all_doctors_screen.dart';
 import '../../modules/users/features/doctor_specialty/logic/cubit/specialty_cubit.dart';
 import '../../modules/users/features/doctor_specialty/ui/doctor_specialty_screen.dart';
 import '../../modules/users/features/doctor_specialty/ui/specialty_doctors_screen.dart';
 import '../../modules/users/features/main_navigation/ui/main_scaffold.dart';
+import '../../modules/users/features/onboarding/ui/on_boarding_screen.dart';
 import '../auth/ui/login_screen.dart';
 import '../auth/ui/sign_up_screen.dart';
 import '../di/dependency_injection.dart';
-import '../../modules/users/features/onboarding/ui/on_boarding_screen.dart';
 import 'routes.dart';
 
 class AppRouter {
@@ -51,7 +53,7 @@ class AppRouter {
             ),
           ),
         );
-      case Routes.recommendationDoctorScreen:
+      case Routes.allDoctorsScreen:
         return MaterialPageRoute(
           builder: (_) => MultiBlocProvider(
             providers: [
@@ -65,6 +67,16 @@ class AppRouter {
               ),
             ],
             child: const AllDoctorsScreen(),
+          ),
+        );
+
+      case Routes.doctorDetailsScreen:
+        return MaterialPageRoute(
+          builder: (final context) => BlocProvider(
+            create: (final context) =>
+                getIt<DoctorDetailsCubit>()
+                  ..showDoctorDetails(args?['doctorId'] as int),
+            child: const DoctorDetailsScreen(),
           ),
         );
 
